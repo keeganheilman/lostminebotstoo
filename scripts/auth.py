e85812a249ddefabec9201385b1697a56c23e5d7
Keegan Heilman <keegan.heilman@gmail.com>
2022-04-01 16:42:01 -0700

    update deprecated tweepy OAth method

--- scripts/auth.py
+++ scripts/auth.py
@@ -1,21 +1,20 @@
-import secrets
 import tweepy
 import psycopg2
-
+from secrets import Secret
 
 def twitter_auth(
         c_key=None,
         c_secret=None,
         a_token=None,
         a_token_secret=None):
-    secret = secrets.Secret()
+    secret = Secret()
     consumer_key = c_key or secret.consumer_key
     consumer_secret = c_secret or secret.consumer_secret
     access_token = a_token or secret.access_token
     access_token_secret = a_token_secret or secret.access_token_secret
-    auth = tweepy.OAuthHandler(
-        consumer_key, consumer_secret,
-        access_token, access_token_secret)
+    
+    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
     return tweepy.API(auth)
 
@@ -26,13 +25,13 @@ def sql_auth(
         s_host=None,
         s_port=None,
         s_db=None):
-    secret = secrets.Secret()
+    secret = Secret()
     sql_user = s_user or secret.sql_username
     sql_pw = s_pw or secret.sql_pw
     sql_host = s_host or secret.sql_host
     sql_port = s_port or secret.sql_port
     sql_db = s_db or secret.sql_db
-
+    cursor=None
     try:
         conn = psycopg2.connect(
             user=sql_user,
@@ -45,3 +44,10 @@ def sql_auth(
         print("Error while connecting to PostgreSQL", error)
 
     return cursor
+
+
+def _test():
+    return False
+
+if __name__ == '__main__':
+    _test()
