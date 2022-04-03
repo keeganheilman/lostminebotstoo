daf72ac087287e7f8fcd811dd9ebaab62dfa10f0
Keegan Heilman <keegan.heilman@gmail.com>
2022-04-02 18:29:03 -0700

    app factory webhook_app structure

---
+++ webhook_app/config.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config(object):
+    """
+    """
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', default='dev')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URI', default='postgres://development...')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', default='postgres://testing...')
+
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URI', default='postgres://production...')
---
+++ webhook_app/webhooks.py
@@ -0,0 +1,39 @@
+import base64
+import hashlib
+import hmac
+import json
+import os
+from flask import Blueprint, request, abort
+from webhook_app.executor import handle_events, is_valid_webhook
+
+bp = Blueprint('webhooks', __name__)
+
+# Defines a route for the POST request
+@bp.route("/", methods=["POST"])
+def handle_webhook():
+    if not is_valid_webhook(request):
+        abort(403)
+    body = request.json
+    if "tweet_create_events" in body:
+        handle_events(body["tweet_create_events"])
+    return "", 204
+
+
+# ref:https://developer.twitter.com/en/docs/twitter-api/enterprise/account-activity-api/guides/securing-webhooks
+# Defines a route for the GET request
+@bp.route('/', methods=['GET'])
+def webhook_challenge():
+    # creates HMAC SHA-256 hash from incomming token and your consumer secret
+    sha256_hash_digest = hmac.new(
+        os.getenv('CONSUMER_KEY', default=''),
+        msg=request.args.get('crc_token'),
+        digestmod=hashlib.sha256
+    ).digest()
+
+    # construct response data with base64 encoded hash
+    response = {
+        'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest)
+    }
+
+    # returns properly formatted json response
+    return json.dumps(response)        
